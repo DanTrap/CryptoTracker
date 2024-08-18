35d877b1c5033f2ce95d5f22715fc3dc45e5f4eb
dantrapdev <dantrapdev@gmail.com>
2024-08-18 12:43:09 +0300

    Remove unused dependency

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -6,6 +6,5 @@ android.namespace = "com.feature.home"
 
 dependencies {
     implementation(project(":core:common"))
-    implementation(libs.material.icons.extended)
     implementation(libs.coil.compose)
 }
