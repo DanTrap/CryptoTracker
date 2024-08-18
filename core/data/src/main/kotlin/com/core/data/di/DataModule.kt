5520bfe2b833ede67c7dbe94ca5734acc4e8bb9e
dantrapdev <dantrapdev@gmail.com>
2024-08-18 15:45:02 +0300

    Replace hardcoded dispatcher with injected

--- core/data/src/main/kotlin/com/core/data/di/DataModule.kt
+++ core/data/src/main/kotlin/com/core/data/di/DataModule.kt
@@ -1,26 +1,14 @@
 package com.core.data.di
 
-import com.core.data.repository.AppReviewManagerImpl
 import com.core.data.repository.CoinsRepositoryImpl
-import com.core.data.repository.ContactUsManagerImpl
-import com.core.data.repository.LanguageRepositoryImpl
 import com.core.data.source.local.CoinsLocalDataSource
 import com.core.data.source.remote.CoinsRemoteDataSource
-import com.core.domain.repository.AppReviewManager
 import com.core.domain.repository.CoinsRepository
-import com.core.domain.repository.ContactUsManager
-import com.core.domain.repository.LanguageRepository
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val dataModule = module {
-
-    singleOf(::LanguageRepositoryImpl) bind LanguageRepository::class
-
-    singleOf(::ContactUsManagerImpl) bind ContactUsManager::class
-
-    singleOf(::AppReviewManagerImpl) bind AppReviewManager::class
 
     singleOf(CoinsLocalDataSource::Base) bind CoinsLocalDataSource::class
 
