c5270f1b3665ba319fb22595e1547010bf25e1a3
dantrapdev <dantrapdev@gmail.com>
2024-08-18 12:33:06 +0300

    Add api method to fetch coin details

--- core/network/src/main/kotlin/com/core/network/api/service/ApiConstants.kt
+++ core/network/src/main/kotlin/com/core/network/api/service/ApiConstants.kt
@@ -4,6 +4,7 @@ internal object ApiConstants {
     object Endpoints {
         const val BASE_URL = "https://api.coingecko.com/api/v3/"
         const val COINS_MARKET_PATH = "coins/markets"
+        const val COIN_DETAILS_PATH = "coins"
     }
 
     object Params {
